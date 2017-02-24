36c555f7046bace2cbc74e8f278b34b51d21e7b9
Sara <saschultz95@gmail.com>
2017-02-24 14:01:30 -0800

    add if statements and program alerts when a number divisible by 3, 5, or 15 is input

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,34 @@
 //Back-end logic:
-var numberToRange = ""
-var output = ""
+var numberToRange = [""]
+var output = [""]
 var resultNumber = [""]
+var newOutput = [""]
 
 var rangeStringNumber = function(numberToRange) {
   for (var i = 1; i <= numberToRange; i ++) {
     resultNumber = (output += i);
-    console.log(resultNumber);
-  }
-return (resultNumber);
-};
+    newOutput = output.slice();
+    // console.log(newOutput);
 
+    }
+    if (numberToRange % 3 === 0) {
+        alert("ping");
+    } else if (numberToRange % 5 === 0) {
+        alert("pong");
+    } else if (numberToRange % 15 === 0) {
+        alert("ping-pong");
+    } else {
+        return newOutput;
+    }
+};
 
 //Front-end logic:
 $(document).ready(function() {
   $("#formOne").submit(function() {
     event.preventDefault();
-    numberToRange = $("#numberInput").val();
+    numberToRange = parseInt($("#numberInput").val());
     output = rangeStringNumber(numberToRange);
         // console.log(numberToRange);
-    $("#result").text(output);
-
-
+    $("#result").text(newOutput);
   });
 });
