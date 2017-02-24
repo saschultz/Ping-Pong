d9ef64db59cb264eb0c2f2686f9998c3ed512a97
Sara <saschultz95@gmail.com>
2017-02-24 11:08:49 -0800

    correct counting start value

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,30 @@
 //Back-end logic:
-var output = "";
-numberToRange = "";
-userInput = "";
+// var output = "";
+// numberToRange = "";
+// userInput = "";
 // var infiniteNum = numberToRange.POSITIVE_INFINITY;
 // console.log(infiniteNum);
 
 
 var rangeStringNumber = function(numberToRange) {
-
-  // for (numberToRange = 2; numberToRange <= Infinity; numberToRange++)
-  //   ("numberToRange" + "numberToRange" + "<br>");
-  //
-  //
-  // if (numberToRange <= 0) {
-  //   return numberToRange;
-  // } else if (numberToRange < Infinity) {
-  //   var returnRange = numberToRange + "00";
-  //   return returnRange;
-  //   console.log(returnRange);
-  // }
-
-  for (var i = 0; i <= numberToRange; i ++) {
-    result += i;
-    console.log(result);;
+  for (var i = 1; i <= numberToRange; i ++) {
+    var newVar = (result += i).slice();
+    // return newVar;
+    console.log(newVar);
   }
 };
+
+// for (numberToRange = 2; numberToRange <= Infinity; numberToRange++)
+//   ("numberToRange" + "numberToRange" + "<br>");
+//
+//
+// if (numberToRange <= 0) {
+//   return numberToRange;
+// } else if (numberToRange < Infinity) {
+//   var returnRange = numberToRange + "00";
+//   return returnRange;
+//   console.log(returnRange);
+// }
 
 
 //Front-end logic:
@@ -32,7 +32,8 @@ $(document).ready(function() {
   $("#formOne").submit(function() {
     event.preventDefault();
     var numberToRange = $("#numberInput").val();
-    output = rangeStringNumber(numberToRange);
+    var output = rangeStringNumber(numberToRange);
+        // console.log(numberToRange);
     $("#result").text(output);
 
 
